4fe0b356bc1e35d2344d6d5b1f5ce34003a29139
lobochimuelo <lobochimuelo96@gmail.com>
2019-02-26 23:10:32 -0400

    Generando Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PageOneComponent } from './components/page-one/page-one.component';
+import { PageTwoComponent } from './components/page-two/page-two.component';
+import { PageThreeComponent } from './components/page-three/page-three.component';
+import { PageFourComponent } from './components/page-four/page-four.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageOneComponent,
+    PageTwoComponent,
+    PageThreeComponent,
+    PageFourComponent
   ],
   imports: [
     BrowserModule
