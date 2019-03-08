9f8448adf5724e599f38f67b46d2397d7d80a824
lobochimuelo <lobochimuelo96@gmail.com>
2019-03-08 17:22:38 -0400

    diseñando componente registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import { PageOneComponent } from './components/page-one/page-one.component';
 import { PageTwoComponent } from './components/page-two/page-two.component';
 import { PageThreeComponent } from './components/page-three/page-three.component';
 import { PageFourComponent } from './components/page-four/page-four.component';
+import { RegistroComponent } from './components/registro/registro.component';
+
 
 const routes: Routes = [
 
@@ -13,6 +15,8 @@ const routes: Routes = [
   { path: 'two', component: PageTwoComponent },
   { path: 'three', component: PageThreeComponent },
   { path: 'four', component: PageFourComponent },
+  { path: 'registro', component:RegistroComponent },
+
   { path: '**', component: PageOneComponent }
 ];
 
