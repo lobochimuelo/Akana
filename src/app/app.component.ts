b2e7a5b663105ba0c576a6df594509a554e5edb1
lobochimuelo <lobochimuelo96@gmail.com>
2019-03-11 21:42:30 -0400

    ocultado menu TU LOCAL para no usuarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { AuthService } from './services/auth.service';
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Akana';
+  constructor(private authService: AuthService, private afsAuth: AngularFireAuth) { }
+  public app_name: string = 'BookStore';
+  public isLogged: boolean = false;
+  ngOnInit() {
+    this.getCurrentUser();
+  }
+
+  getCurrentUser() {
+    this.authService.isAuth().subscribe(auth => {
+      if (auth) {
+        console.log('user logged');
+        this.isLogged = true;
+      } else {
+        console.log('NOT user logged');
+        this.isLogged = false;
+      }
+    });
+  }
+
+  onLogout() {
+    this.afsAuth.auth.signOut();
+  }
 }
